8de5690a0479887aa00abd592f96aa306aa740a6
winiarcc <winiarcc@lafayette.edu>
2018-04-30 16:52:27 -0400

    Simplified some expressions by using flatmap, started adding some comments

--- metadata.js
+++ metadata.js
@@ -1,94 +1,144 @@
-const bacon = require("baconjs")
-const request = require("request")
-const events = require("events")
-const fetch = require("node-fetch")
-const _ = require("lodash") // Utility Functions
-const eventEmitter = new events.EventEmitter()
+//First, we will use require() to grab our dependencies
+const bacon = require("baconjs")	// baconjs is our streaming library,
+									// streams provides a datastructure for dealing with a series of
+									// asynchronous events, which is incredibly useful in this context 
+									// https://baconjs.github.io/index.html
+									// 
+const fetch = require("node-fetch")	// node-fetch is an implementation of the fetch api in nodejs
+									// it is really useful for calling external web APIs
+									// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+									// 
+const _ = require("lodash")			// lodash is a popular library providing a number of useful functions
+									// not included in vanilla javascript. We are using it for its 
+									// .equals() and .get() functions
+									// https://lodash.com/ 
 
+// For convenience, we will store the URLs of the api's we intend to use as constants
+
+// Teal is WJRH's current DJ management system. Among other things, it provides
+// an api for finding out the name of the current show, and the songs they are playing
 const tealURL = "https://api.teal.cool/organizations/wjrh/latest"
+
+// If no show is currently playing, WJRH will switch to music from it's robo-dj program
+// TODO: <include hyperlink to robo-dj github>
+// information about the current song and artist on robo-dj can be found at this URL
 const iceCastURL = "http://www.wjrh.org:8000/status-json.xsl"
 
+/**
+ * LastFM is a service for looking up detailed Metadata information about a given song.
+ * They provide a good API for searching a song by its trackname and artistname,
+ * and contain a wealth of information about almost any given song, although we are primarily only
+ * using it to find links to Album Artwork
+ * 
+ * https://www.last.fm/home
+ * https://www.last.fm/api/intro
+ *
+ * Since this API is a little more involved, we will write a function to produce the URL
+ * for us, given any trackName and artistName
+ * 
+ * @param  {string} trackName  The name of the song to look up
+ * @param  {string} artistName The name of the song's artist
+ * @return {string}            The URL we will use to make our API call
+ */
 const lastFM_ApiString = function(trackName, artistName){
-	const LASTFM_API_KEY = "14cacc2d28210dcd318ffa2085778844"
+	const LASTFM_API_KEY = "14cacc2d28210dcd318ffa2085778844"	// out LastFm api key, if a new one is needed for any reason
+																// replace it here
 	return "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=" + LASTFM_API_KEY 
-	+ "&artist=" + encodeURI(artistName.replace(" ","+"))
-	+ "&track=" + encodeURI(trackName.replace(" ","+"))
+	+ "&artist=" + encodeURI(artistName.replace(" ","+"))	// URI encode the artist and trackname
+	+ "&track=" + encodeURI(trackName.replace(" ","+"))		// Also, replace spaces with "+"s. 
+															// This is a convention defined by LastFm
 	+ "&format=json"
 }
 
+/**
+ * Make a call to Teal and return a promise containing the result
+ * @return {Promise} The result of the API call
+ */
 const tealCall = function(){
-	fetch(tealURL)
-	.then(res => res.json())
-	.then(body => body.event == "episode-end" ? null : body)
-	.then(data => eventEmitter.emit("tealResponse", data))
-	.catch(err => eventEmitter.emit("tealResponse", err))
+	return fetch(tealURL)
+		.then(res => res.json())	// if successful, resolve into the body in json format
+		.catch(err => err.message)	// if there was an error, resolve into the error message
 }
 
+/**
+ * Make a call to Icecast and return a promise containing the result
+ * @return {Promise} The result of the API call
+ */
 const iceCastCall = function(){
-	fetch(iceCastURL)
-	.then(res => res.json())
-	.then(data => data.icestats.source[0].title.split(" - "))
-	.then(data => data.map(element => element.replace(/\[.*\]/, "").trim()))
-	.then(data => {
-		return {
-			track: {
-				title: data[0],
-				artist: data[1]
-			}
-		}
-	})
-	.then(data => eventEmitter.emit("iceCastResponse", data))
-	.catch(err => eventEmitter.emit("iceCastResponse", err))
+	return fetch(iceCastURL)
+		.then(res => res.json())	// if successful, resolve into the body in json format
+		.catch(err => err.message)	// if there was an error, resolve into the error message
 }
 
+/**
+ * Make a call to LastFm and return a promise containing the result
+ * @return {Promise} The result of the API call
+ */
 const lastFmCall = function(trackName, artistName){
-	fetch(lastFM_ApiString(trackName || "", artistName || ""))
-	.then(res => res.json())
-	.then(body => eventEmitter.emit("lastFmResponse", body))
-	.catch(err => eventEmitter.emit("lastFmResponse", err.message))
+	return fetch(lastFM_ApiString(trackName || "", artistName || ""))	// if either field is null, replace it with an empty string
+																		// this prevents crashing
+		.then(res => res.json())	// if successful, resolve into the body in json format
+		.catch(err => err.message)	// if there was an error, resolve into the error message
 }
 
-setInterval(tealCall, 100)
-setInterval(iceCastCall, 100)
+//Now, we will start to create our streams. Each one represents a series of asynchronous events
 
-const tealEvents = bacon.fromEvent(eventEmitter, "tealResponse")
-const iceCastEvents = bacon.fromEvent(eventEmitter, "iceCastResponse")
-const lastFmEvents = bacon.fromEvent(eventEmitter, "lastFmResponse")
 
-const nowPlaying = tealEvents.zip(iceCastEvents, function(teal, iceCast){
-	return teal || iceCast
-}).skipDuplicates(_.isEqual)
+/**
+ * 
+ * @type {[type]}
+ */
+const tealStream = bacon.fromPoll(100, tealCall)
+.flatMap(bacon.fromPromise)
+.map(body => body.event == "episode-end" ? null : body)
 
-nowPlaying.onValue(newData => {
-	lastFmCall(_.get(newData,"track.title"), _.get(newData,"track.artist"))
-})
+const iceCastStream = bacon.fromPoll(100, iceCastCall)
+.flatMap(bacon.fromPromise)
+.map(data => data.icestats.source[0].title)
+.map(title => title.replace(/\[.*\]/, "").trim())
+.map(title => title.split(" - "))
+.map(data => {return {
+	title: data[0], 
+	artist: data[1]
+}})
 
-const metadata = nowPlaying.zip(lastFmEvents, function(teal, lastFm){
-	let data = {teal: teal, lastFm: lastFm}
+const nowPlaying = bacon.when(
+	[tealStream, iceCastStream], (teal, iceCast) => teal || iceCast
+).skipDuplicates(_.isEqual)
+.log()
 
-	return {
-		songname: chooseFrom([
-			"lastFm.track.name",
-			"teal.track.title"
-		], "Song Unknown")(data),
-		artistname: chooseFrom([
-			"lastFm.track.artist.name",
-			"teal.track.artist"
-		], "Artist Unknown")(data),
-		title: chooseFrom([
-			"teal.program.name"
-		], "RoboDJ's greatest hits")(data),
-		author: chooseFrom([
-			"teal.program.author"
-		], "RoboDJ")(data),
-		image: chooseFrom([
-			"lastFm.track.album.image[3].#text",
-			"teal.episode.image",
-			"teal.program.image"
-		 ], "https://media.giphy.com/media/kIjnPxW8ESIqQ/giphy.gif"
-		)(data)
+lastFmEvents = nowPlaying
+.map(track => lastFmCall(track.title, track.artist))
+.flatMap(bacon.fromPromise)
+
+const metadata = bacon.when(
+	[nowPlaying, lastFmEvents], function(teal, lastFm){
+		let data = {teal: teal, lastFm: lastFm}
+
+		return {
+			songname: chooseFrom(data)([
+				"lastFm.track.name",
+				"teal.track.title"
+			], "Song Unknown"),
+			artistname: chooseFrom(data)([
+				"lastFm.track.artist.name",
+				"teal.track.artist"
+			], "Artist Unknown"),
+			title: chooseFrom(data)([
+				"teal.program.name"
+			], "RoboDJ's greatest hits"),
+			author: chooseFrom(data)([
+				"teal.program.author"
+			], "RoboDJ"),
+			image: chooseFrom(data)([
+				"lastFm.track.album.image[3].#text",
+				"teal.episode.image",
+				"teal.program.image"
+			 ], "https://media.giphy.com/media/kIjnPxW8ESIqQ/giphy.gif"
+			)
+		}
 	}
-})
+)
 
 metadata.log()
 
@@ -102,7 +152,7 @@ exports.getData = function(key) {
 
 exports.onData = func => metadata.onValue(func)
 
-const chooseFrom = (paths, Default) => obj => {
+const chooseFrom = obj => (paths, Default) => {
 	return paths.reduce((prev, cur) => {
 		return prev || _.get(obj, cur)
 	}, _.get(obj, paths[0])) || Default
